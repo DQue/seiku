54c19f249131a584d19a82a20ae0fe9637705f37
DQue <xxdqxx@gmail.com>
2020-12-13 15:59:39 +0900

    E-4は重爆らしい　乙以下要検証

--- seiku.js
+++ seiku.js
@@ -2498,6 +2498,7 @@ function 二_防空結果テーブルを生成() {
 		if (O.kai === "四国沖/九州西方五島列島沖") jf = true;
 		if (O.kai === "小笠原諸島沖") jf = true;
 		if (O.kai === "ノルウェー北岬沖/北極海") jf = true;
+		if (O.hou === "護衛せよ！船団輸送作戦《甲》" && O.kai === "ルソン島沖/オルモック沖") jf = true;
 		if (jf) {
 			重爆空襲か[j] = true;
 		}
